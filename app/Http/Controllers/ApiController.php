d1d8c5999dd96afd4f01ec3849a5da4326df2b70
Edward-MAC <inyeob1989@gmail.com>
2019-08-23 11:30:20 -0700

    Api changed

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use http\Env\Response;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    /**
+     * Create a new authentication controller instance
+    */
+    public function __construct()
+    {
+        $this -> beforeFilter('auth',['on'=>'post']);
+    }
+
+    protected $statusCode = Response::HTTP_OK;
+
+    public function getStatusCode()
+    {
+        return $this-> statusCode;
+    }
+
 }
